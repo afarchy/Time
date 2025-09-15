149f1c0e0e2bfe41b2aa5cb73a4a5340425a3d36
Alon Farchy <afarchy@hotmail.com>
2025-09-15 10:51:29 -0500

    Initial Commit

---
+++ Time/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Time
+//
+//  Created by Alon Farchy on 9/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TimeTests/TimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTests.swift
+//  TimeTests
+//
+//  Created by Alon Farchy on 9/15/25.
+//
+
+import Testing
+@testable import Time
+
+struct TimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
